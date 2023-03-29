f7c6708277db3bd4018bebdf2b150da5b1e61ea7
Elie Gauthey <egauthey@c2r4s1.42lausanne.ch>
2023-03-29 17:22:59 +0200

    error handling in CPP01 and almost finished ex03 CPP03

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -10,6 +10,11 @@ int	main(int ac, char **av)
 		std::cerr << "Usage :" << av[0] << " <filename> <string to replace> <replacement string>" << std::endl;
 		return 1;
 	}
+	if (av[2] == std::string(""))
+	{
+		std::cerr << "<string to replace> can not be empty." << std::endl;
+		return 2;
+	}
 	FileHandler	fileHandler(av[1]);
 	StringRep	stringRep(av[2], av[3], fileHandler.getInfileStr());
 	fileHandler.copyStrInOutfileStream(stringRep.getOutfileStr());
