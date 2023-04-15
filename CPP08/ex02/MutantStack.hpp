7805aeb010cf1922e5ca0a153f4a2e2cfece7041
Elie Gauthey <egauthey@c2r4s1.42lausanne.ch>
2023-04-15 16:32:52 +0200

    finishe CPP08

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,22 +1,98 @@
 #include <iostream>
 #include <stack>
 
-#ifndef MutantStack_HPP
-# define MutantStack_HPP
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
-template <typename T>
+#include <stack>
+#include <deque>
+
+template <class T>
 class MutantStack : public std::stack<T> {
 
 public:
 
-	MutantStack();
-	MutantStack(const MutantStack& src);
-	~MutantStack();
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack& src) : std::stack<T>(src), _helpContainer(src._helpContainer) {}
+	~MutantStack() {}
 
-	MutantStack& operator=(const MutantStack& rhs);
+	MutantStack& operator=(const MutantStack& rhs) {
+		if (this != &rhs) {
+			std::stack<T>::operator=(rhs);
+			_helpContainer = rhs._helpContainer;
+		}
+		return *this;
+	}
+
+	typedef typename std::deque<T>::iterator		iter;
+
+	// In C++ templates, you encounter types that are dependent on a template parameter :
+	// When a type is dependant on a template, the compiler has difficulty determining if an identifier is a type or a value.
+	// typename keyword is used to tell the compiler that  std::deque<T>::iterator is a type.
+
+	typedef typename std::deque<T>::const_iterator	constIter;
+
+	iter begin() {
+		return _helpContainer.begin();
+	}
+
+	iter end() {
+		return _helpContainer.end();
+	}
+
+	constIter begin() const {
+		return _helpContainer.begin();
+	}
+
+	constIter end() const {
+		return _helpContainer.end();
+	}
+
+	void push(const T& value) {
+		std::stack<T>::push(value);
+		_helpContainer.push_back(value);
+	}
+
+	void pop() {
+		if (!std::stack<T>::empty()) {
+			std::stack<T>::pop();
+			_helpContainer.pop_back();
+		}
+	}
+
+// 	typedef typename std::stack<T>::container_type::iterator 		it;
+
+// 	// In C++ templates, you encounter types that are dependent on a template parameter :
+// 	// When a type is dependant on a template, the compiler has difficulty determining if an identifier is a type or a value.
+// 	// typename keyword is used to tell the compiler that std::stack<T>::container_type::iterator is a type.
+
+// 	// std::stack<T>::container_type -> is a typedef of std::stack<T> class
+// 	// that represent the type of the UNDERLYING container used to implement the stack (std::deque<T>)
+// 	// Since T is a template parameter, container_type is a dependent type that depends on the type of T.
+
+// 	// std::stack<T>::container_type::iterator -> represent the iterator type. An iterator of type std::stack<T>::containter_type
+// 	// is also a dependent type, as it depends on the type of the underlying container which in turn depends on T.
+
+// 	typedef typename std::stack<T>::container_type::const_iterator	const_it;
+
+// 	it	begin() {
+// 		return this->c.begin(); // this->c refers to the underlying container of the std::stack<T>
+// 	}
+
+// 	it	end() {
+// 		return this->c.end();
+// 	}
+
+// 	const_it	beginConst() {
+// 		return this->c.begin();
+// 	}
+
+// 	const_it	endConst() {
+// 		return this->c.end();
+// 	}
 
 private:
-
+	std::deque<T> _helpContainer;
 };
 
 #endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,7 +1,130 @@
 #include <iostream>
 #include "MutantStack.hpp"
+#include <list>
 
 int main( void )
 {
+	// MutantStack<int> mstack;
+	// mstack.push(5);
+	// mstack.push(17);
+
+	// std::cout << mstack.top() << std::endl;
+	// mstack.pop();
+	// std::cout << mstack.size() << std::endl;
+	// mstack.push(3);
+	// mstack.push(5);
+	// mstack.push(737);
+	
+	// mstack.push(0);
+	// MutantStack<int>::iter it = mstack.begin();
+	// MutantStack<int>::iter ite = mstack.end();
+	// ++it;
+	// --it;
+	// while (it != ite) {
+	// 	std::cout << *it << std::endl;
+	// 	++it;
+	// }
+	// std::stack<int> s(mstack);
+
+	 // Test basic functionality
+	MutantStack<int> ms;
+	ms.push(10);
+	ms.push(20);
+	ms.push(30);
+
+	std::cout << "Test basic functionality" << std::endl;
+	for (MutantStack<int>::iter it = ms.begin(); it != ms.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	// Test copy constructor
+	MutantStack<int> ms2(ms);
+
+	std::cout << "Test copy constructor" << std::endl;
+	for (MutantStack<int>::iter it = ms2.begin(); it != ms2.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	// Test assignment operator
+	MutantStack<int> ms3;
+	ms3 = ms;
+
+	std::cout << "Test assignment operator" << std::endl;
+	for (MutantStack<int>::iter it = ms3.begin(); it != ms3.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	// Test pop
+	ms.pop();
+
+	std::cout << "Test pop" << std::endl;
+	for (MutantStack<int>::iter it = ms.begin(); it != ms.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
+	// Test with double
+	MutantStack<double> ms4;
+	ms4.push(1.5);
+	ms4.push(2.5);
+	ms4.push(3.5);
+
+	std::cout << "Test with double)" << std::endl;
+	for (MutantStack<double>::iter it = ms4.begin(); it != ms4.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl << std::endl;
+
 	return 0;
 }
+
+
+// SAME MAIN WITH LIST CONTAINER
+// int main( void )
+// {
+// 	std::list<int> list;
+// 	list.push_back(10);
+// 	list.push_back(20);
+// 	list.push_back(30);
+
+// 	std::cout << "Test basic functionality" << std::endl;
+// 	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl << std::endl;
+
+// 	// Test copy constructor
+// 	std::list<int> list2(list);
+
+// 	std::cout << "Test copy constructor" << std::endl;
+// 	for (std::list<int>::iterator it = list2.begin(); it != list2.end(); ++it)
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl << std::endl;
+
+// 	// Test assignment operator
+// 	std::list<int> list3;
+// 	list3 = list;
+
+// 	std::cout << "Test assignment operator" << std::endl;
+// 	for (std::list<int>::iterator it = list3.begin(); it != list3.end(); ++it)
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl << std::endl;
+
+// 	// Test pop
+// 	list.pop_back();
+
+// 	std::cout << "Test pop" << std::endl;
+// 	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl << std::endl;
+
+// 	// Test with double
+// 	std::list<double> list4;
+// 	list4.push_back(1.5);
+// 	list4.push_back(2.5);
+// 	list4.push_back(3.5);
+
+// 	std::cout << "Test with double)" << std::endl;
+// 	for (std::list<double>::iterator it = list4.begin(); it != list4.end(); ++it)
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl << std::endl;
+
+// 	return 0;
+// }
